e757327234a695263442ee025a146b486e42bda5
Utsav Phunyal <uphunyal@troy.edu>
2020-12-14 17:57:08 -0500

    Add project files.

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index(string category, int productPage=1) => View( new ProductsListViewModel
+        {
+            Products= repository.Products
+            .Where(p=>category==null ||p.Category==category)
+            .OrderBy(p=>p.ProductID)
+            .Skip((productPage-1)*PageSize)
+            .Take(PageSize),
+            PagingInfo= new PagingInfo
+            {
+                CurrentPage= productPage,
+                ItemsPerPage= PageSize,
+                TotalItems= repository.Products.Count()
+            }, 
+            CurrentCategory=category
+        });
+    }  
+}
---
+++ SportsStore/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+
+       
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        
+        }
+        private IConfiguration Configuration { get; set; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:SportsStoreConnection"]);
+            });
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", action = "Index" });
+                endpoints.MapDefaultControllerRoute();
+            });
+            SeedData.EnsurePopulated(app);
+        }
+    }
+}
