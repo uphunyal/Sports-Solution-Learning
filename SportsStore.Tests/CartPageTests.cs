81f227ed27f322cda4e85107b2118dfc47a0a6e2
Utsav Phunyal <uphunyal@troy.edu>
2020-12-19 03:35:46 -0500

    Cart Page unit test

---
+++ SportsStore.Tests/CartPageTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using SportsStore.Models;
+using SportsStore.Pages;
+using System;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class CartPageTests
+    {
+        
+        [Fact]
+        public void Can_Load_Cart()
+        {
+            //Arrange - create mock repository
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Mock<IStoreRepository> mockRepo = new Mock<IStoreRepository>();
+            mockRepo.Setup(m => m.Products).Returns((new Product[] { p1, p2 }).AsQueryable<Product>());
+
+            //Create cart
+            Cart testCart = new Cart();
+            testCart.AddItem(p1, 2);
+            testCart.AddItem(p2, 1);
+
+            //Create a mock page context and session
+            Mock<ISession> mockSession = new Mock<ISession>();
+            byte[] data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(testCart));
+            mockSession.Setup(c => c.TryGetValue(It.IsAny<String>(), out data));
+            Mock<HttpContext> mockContext = new Mock<HttpContext>();
+            mockContext.SetupGet(c => c.Session).Returns(mockSession.Object);
+
+            //Action
+            CartModel cartModel = new CartModel(mockRepo.Object)
+            {
+                PageContext = new PageContext(new ActionContext
+                {
+                    HttpContext = mockContext.Object,
+                    RouteData = new RouteData(),
+                    ActionDescriptor = new PageActionDescriptor()
+                })
+            };
+            cartModel.OnGet("myUrl");
+
+            //Assert
+            //Assert
+            Assert.Equal(2, cartModel.Cart.Lines.Count());
+            Assert.Equal("myUrl", cartModel.ReturnUrl);
+        }
+        }
+    }
+}
