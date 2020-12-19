8cded2e9d61ff4b8d1d5dc003466bbcb8097dc1b
Utsav Phunyal <uphunyal@troy.edu>
2020-12-18 21:30:04 -0500

    Extension method to track session data

---
+++ SportsStore/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SportsStore.Infrastructure
+{
+    //Extension for Session state data , Cart object, serialize and deserialize objects to json and vice versa
+    public static class SessionExtensions
+    {
+        public static void SetJson( this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T GetJson<T> (this ISession session, string key)
+        {
+            var sessionData = session.GetString(key);
+            return sessionData == null ? default(T) : JsonSerializer.Deserialize<T>(sessionData);
+        }
+    }
+}
