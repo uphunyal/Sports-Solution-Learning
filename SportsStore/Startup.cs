b4169359c3d96b518c81d0a1c6fdabfb6916df3a
Utsav Phunyal <uphunyal@troy.edu>
2020-12-18 20:52:51 -0500

    More Setting up for cart functionality

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -34,6 +34,8 @@ namespace SportsStore
             });
             services.AddScoped<IStoreRepository, EFStoreRepository>();
             services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +49,7 @@ namespace SportsStore
             app.UseRouting();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
